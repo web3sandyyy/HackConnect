ecfd3ee91f9c932ccbc74118d4293bf402dc3e39
web3sandyyy <sandeeprajapati18@gmail.com>
2024-12-07 03:24:22 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      This is SignUp Page
+    </div>
+  )
+}
+
+export default SignUp
